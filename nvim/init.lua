02818b5fe1c197167877e7c5e23eef70b5aadb15
バカです <naimjonn.nn@live.ru>
2023-10-24 21:15:22 +0300

    `PACKER.NVIM -> LAZY.NVIM`

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,10 +1,24 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+
 ------------------------------------------------------------------
 -- Import files
 ------------------------------------------------------------------
 
 require('general')
 require('plugins')
-require('plugin_settings')
+require('plugins_settings')
 require('keymaps')
 require('lang_settings')
 require('settings')
