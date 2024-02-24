de677168928682a1a5a6c6221a1382bda62af32c
バカです <naimjonn.nn@live.ru>
2024-02-24 22:32:40 +0300

    Added some `chars`, 20 IS NOT enough

--- i3/scripts/wmfocus.sh
+++ i3/scripts/wmfocus.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 # wmfocus --textcolor "#fb4934" --bgcolor "#1d2021" --valign center -f "DejaVu Nerd Font Bold":100 -c nthgcrpbwvasyfloeuid --offset 2,0 --halign center
-wmfocus --textcolor "#fb4934" --bgcolor "#1d2021" -f "DejaVu Nerd Font Bold":100 -c nthgcrpbwvasyfloeuid --offset 2,0 --halign center
+wmfocus --textcolor "#fb4934" --bgcolor "#1d2021" -f "DejaVu Nerd Font Bold":100 -c ntheuidgmcrpbwvasyfloz --offset 2,0 --halign center
