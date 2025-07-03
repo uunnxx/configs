9b51401b101ac0321142a09a4ccdc1ff1a9ea78c
バカです <naimjonn.nn@live.ru>
2025-07-03 11:06:39 +0300

    ref: updated to new version

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,18 +1,3 @@
--- local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
--- if not vim.loop.fs_stat(lazypath) then
---   vim.fn.system({
---     "git",
---     "clone",
---     "--filter=blob:none",
---     "https://github.com/folke/lazy.nvim.git",
---     "--branch=stable", -- latest stable release
---     lazypath,
---   })
--- end
--- vim.opt.rtp:prepend(lazypath)
-
-
--- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
@@ -46,11 +31,13 @@ require('keymaps')
 require('lang_settings')
 require('settings')
 
-require('plugin_configs/completion')
-require('plugin_configs/gitsigns')
-require('plugin_configs/debugging')
-require('plugin_configs/lsp_configs')
-require('plugin_configs/lualine')
+require('plugin_configs.completion')
+require('plugin_configs.gitsigns')
+require('plugin_configs.debugging')
+require('plugin_configs.lsp_configs')
+require('plugin_configs.lualine')
 -- require('plugin_configs/nui')
-require('plugin_configs/telescope')
-require('plugin_configs/ui')
+require('plugin_configs.telescope')
+require('plugin_configs.ui')
+
+require('plugin_configs.cloak')
