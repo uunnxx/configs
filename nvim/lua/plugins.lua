634b3c87b805c824bd9cd617b48d758951c2c468
バカです <naimjonn.nn@live.ru>
2025-07-03 11:09:41 +0300

    added: `cloak` plugin for sensitive .env files

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -216,20 +216,47 @@ return require('lazy').setup({
         dependencies = "nvim-tree/nvim-web-devicons",
         opts = {},
         cmd = "Trouble",
-        -- config = function()
-        --     require("trouble").setup {}
-        -- end
         keys = {
             {
                 "gT",
                 "<cmd>Trouble<cr>",
                 desc = 'Trouble'
             },
-            {
-                "gR",
-                "<cmd>Trouble lsp_references toggle<cr>",
-                desc = 'LSP References (Trouble)'
-            }
+            -- {
+            --     "gR",
+            --     "<cmd>Trouble lsp_references toggle<cr>",
+            --     desc = 'LSP References (Trouble)'
+            -- },
+            -- {
+            --     "<leader>xx",
+            --     "<cmd>Trouble diagnostics toggle<cr>",
+            --     desc = "Diagnostics (Trouble)",
+            -- },
+            -- {
+            --     "<leader>xX",
+            --     "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+            --     desc = "Buffer Diagnostics (Trouble)",
+            -- },
+            -- {
+            --     "<leader>cs",
+            --     "<cmd>Trouble symbols toggle focus=false<cr>",
+            --     desc = "Symbols (Trouble)",
+            -- },
+            -- {
+            --     "<leader>cl",
+            --     "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+            --     desc = "LSP Definitions / references / ... (Trouble)",
+            -- },
+            -- {
+            --     "<leader>xL",
+            --     "<cmd>Trouble loclist toggle<cr>",
+            --     desc = "Location List (Trouble)",
+            -- },
+            -- {
+            --     "<leader>xQ",
+            --     "<cmd>Trouble qflist toggle<cr>",
+            --     desc = "Quickfix List (Trouble)",
+            -- },
         }
     },
 
@@ -390,5 +417,9 @@ return require('lazy').setup({
         event = "VeryLazy",
         ft = {'html', 'css', 'erb', 'htmldjango', 'html5'}
 
-    }
+    },
+
+    {'https://github.com/laytan/cloak.nvim'}
 })
+
+
