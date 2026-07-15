8015c8439c3f49e61f751163920aa9bf656b03fd
バカです <naimjonn.nn@live.ru>
2026-07-16 02:20:27 +0300

    added: `markit` plugin, but keep it as empty `table`

---
+++ nvim/lua/plugins/markit.lua
@@ -0,0 +1,6 @@
+return {
+	-- "2kabhishek/markit.nvim",
+	-- dependencies = { "2kabhishek/pickme.nvim", "nvim-lua/plenary.nvim" },
+	-- opts = {}, -- Add your configuration here, required if you are not calling markit.setup manually elsewhere
+	-- event = { "BufReadPre", "BufNewFile" },
+}
