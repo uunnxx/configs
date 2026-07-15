1f41d5c6ee364eee8b1209455ec3b1c87f714164
バカです <naimjonn.nn@live.ru>
2026-07-16 02:46:34 +0300

    added: `keymap` for flash and `navic` now has full nerd icons

--- nvim/lua/core/plugins_settings.lua
+++ nvim/lua/core/plugins_settings.lua
@@ -28,8 +28,6 @@ require("vim._core.ui2").enable({
 	},
 })
 
-
-
 -------------------------------------------------------------------------------
 -- tpope's surround plugin
 -- Sv or S{ for a variable
@@ -50,3 +48,100 @@ require("vim._core.ui2").enable({
 --     let b:surround_{char2nr('f')} = '{% for \1for loop: \1 %}\r{% endfor %}'
 --     let b:surround_{char2nr('c')} = '{% comment %}\r{% endcomment %}'
 -- ]])
+
+-- Flash HopWord
+vim.keymap.set({ "n", "x", "o" }, ",m", function()
+	local Flash = require("flash")
+
+	---@param opts Flash.Format
+	local function format_first_match(opts)
+		-- always show first and second label
+		return {
+			{ opts.match.label1, opts.hl_group },
+			{ opts.match.label2, opts.hl_group },
+		}
+	end
+
+	local function format_second_match(opts)
+		return {
+			{ opts.match.label2, opts.hl_group },
+		}
+	end
+
+	Flash.jump({
+		search = { mode = "search" },
+		label = { after = false, before = { 0, 0 }, uppercase = false, format = format_first_match },
+		pattern = [[\<]],
+		action = function(match, state)
+			state:hide()
+			Flash.jump({
+				search = { max_length = 0 },
+				highlight = { matches = false },
+				label = { after = { 0, 2 }, format = format_second_match },
+				matcher = function(win)
+					-- limit matches to the current label
+					return vim.tbl_filter(function(m)
+						return m.label == match.label and m.win == win
+					end, state.results)
+				end,
+				labeler = function(matches)
+					for _, m in ipairs(matches) do
+						m.label = m.label2 -- use the second label
+					end
+				end,
+			})
+		end,
+		labeler = function(matches, state)
+			local labels = state:labels()
+			for m, match in ipairs(matches) do
+				match.label1 = labels[math.floor((m - 1) / #labels) + 1]
+				match.label2 = labels[(m - 1) % #labels + 1]
+				match.label = match.label1
+			end
+		end,
+	})
+end)
+
+-- vim.keymap.set({ "n", "x", "o" }, "lf", function()
+-- 	require("flash").jump({
+-- 		search = { mode = "search", max_length = 0 },
+-- 		label = { after = { 0, 0 } },
+-- 		pattern = "^",
+-- 	})
+-- end)
+
+local navic = require("nvim-navic")
+navic.setup({
+	icons = {
+		File = "󰈙 ",
+		Module = " ",
+		Namespace = "󰌗 ",
+		Package = " ",
+		Class = "󰌗 ",
+		Method = "󰆧 ",
+		Property = " ",
+		Field = " ",
+		Constructor = " ",
+		Enum = "󰕘",
+		Interface = "󰕘",
+		Function = "󰊕 ",
+		Variable = "󰆧 ",
+		Constant = "󰏿 ",
+		String = "󰀬 ",
+		Number = "󰎠 ",
+		Boolean = "◩ ",
+		Array = "󰅪 ",
+		Object = "󰅩 ",
+		Key = "󰌋 ",
+		Null = "󰟢 ",
+		EnumMember = " ",
+		Struct = "󰌗 ",
+		Event = " ",
+		Operator = "󰆕 ",
+		TypeParameter = "󰊄 ",
+		enabled = true,
+	},
+
+	separator = "%#NavicSeparator#  %*",
+	highlight = true,
+})
