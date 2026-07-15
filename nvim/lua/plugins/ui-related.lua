85cae54e58ab5e79d77a6f4adeba56730252f6e3
バカです <naimjonn.nn@live.ru>
2026-07-16 02:36:39 +0300

    ref: cleaned

--- nvim/lua/plugins/ui-related.lua
+++ nvim/lua/plugins/ui-related.lua
@@ -5,11 +5,11 @@ return {
 	{
 		-- Simple winbar/statusline plugin that shows your current code context
 		"SmiteshP/nvim-navic",
-	},
-	{ "akinsho/bufferline.nvim" },
-	{
-		"NvChad/nvim-colorizer.lua",
-		event = "BufReadPre",
-		opts = {},
+		{ "akinsho/bufferline.nvim" },
+		{
+			"NvChad/nvim-colorizer.lua",
+			event = "BufReadPre",
+			opts = {},
+		},
 	},
 }
